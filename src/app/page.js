5bfdf4c992ad046b26ff59770647b4cfcf241ab2
Huy-pynok3 <huy53497@gmail.com>
2025-06-26 11:15:57 +0700

    feat: add Service

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Style from "@/styles/page.module.css";
-import { HeroSection } from "@/components/componentsindex";
+import { HeroSection, Service } from "@/components/componentsindex";
 
 export default function Home() {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
+      <Service />
     </div>
   );
 }
