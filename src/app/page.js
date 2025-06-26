1b71198fd11957b8445be19f2649f1f3d15df83a
Huy-pynok3 <huy53497@gmail.com>
2025-06-26 12:10:48 +0700

    feat: add BigNFTSlider

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Style from "@/styles/page.module.css";
-import { HeroSection, Service } from "@/components/componentsindex";
+import { BigNFTSlider, HeroSection, Service } from "@/components/componentsindex";
 
 export default function Home() {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
-      <Service />
+      <Service/>
+      <BigNFTSlider/>
     </div>
   );
 }
