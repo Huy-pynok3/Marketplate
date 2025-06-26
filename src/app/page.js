beeb16879529604aaf90e4127898cdf7b5a0522e
Huy-pynok3 <huy53497@gmail.com>
2025-06-26 13:44:28 +0700

    feat: add Subscribe,Title,Category

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Style from "@/styles/page.module.css";
-import { BigNFTSlider, HeroSection, Service } from "@/components/componentsindex";
+import {
+    BigNFTSlider,
+    HeroSection,
+    Service,
+    Subscribe,
+    Title,
+    Category
+} from "@/components/componentsindex";
 
 export default function Home() {
-  return (
-    <div className={Style.homePage}>
-      <HeroSection/>
-      <Service/>
-      <BigNFTSlider/>
-    </div>
-  );
+    return (
+        <div className={Style.homePage}>
+            <HeroSection />
+            <Service />
+            <BigNFTSlider />
+            <Title
+                heading="Browse by category"
+                paragraph="Explore the NFTs in the most featured categories."
+            />
+            <Category />
+            <Subscribe />
+        </div>
+    );
 }
